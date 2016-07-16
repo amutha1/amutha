2467abe15b88177001e74cdded679d296209c63d
amutha1 <amuthakumaresan1996@gmail.com>
2016-07-16 02:46:57 -0700

    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("given no. is positive");
+}
+else
+{
+printf("given no. is negative");
+}
+}
